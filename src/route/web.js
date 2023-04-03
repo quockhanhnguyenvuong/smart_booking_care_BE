85132e7e688da394414a13719bc44231aab1929e
Hai Tran <haihiv2205@gmail.com>
2023-04-03 23:17:46 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,32 @@
+import express from "express";
+import userController from "../controllers/userController";
+import homeController from "../controllers/homeController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+  router.get("/", homeController.getHomePage);
+  router.get("/about", homeController.getAboutPage);
+
+  // API create new user
+  router.get("/crud", homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
+
+  //API read user
+  router.get("/get-crud", homeController.displayGetCRUD);
+
+  //API update user
+  router.get("/edit-crud", homeController.getEditCRUD);
+  router.post("/put-crud", homeController.putCRUD);
+
+  //API delete user
+  router.get("/delete-crud", homeController.deleteCRUD);
+
+ 
+
+  router.post('/api/login', userController.handleLogin);
+
+  return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
